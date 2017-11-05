ce6650586a1107e1b056779d90acfdc2d8ccd390
Kasper Tvede <kasper@webmasteren.eu>
2017-11-05 21:07:01 +0100

    init project setup

---
+++ csenseIosBase/csenseIosBase.h
@@ -0,0 +1,19 @@
+//
+//  csenseIosBase.h
+//  csenseIosBase
+//
+//  Created by Kasper T on 03/11/2017.
+//  Copyright © 2017 commonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for csenseIosBase.
+FOUNDATION_EXPORT double csenseIosBaseVersionNumber;
+
+//! Project version string for csenseIosBase.
+FOUNDATION_EXPORT const unsigned char csenseIosBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <csenseIosBase/PublicHeader.h>
+
+
---
+++ csenseIosBaseTests/csenseIosBaseTests.swift
@@ -0,0 +1,25 @@
+//
+//  csenseIosBaseTests.swift
+//  csenseIosBaseTests
+//
+//  Created by Kasper T on 03/11/2017.
+//  Copyright © 2017 commonsense. All rights reserved.
+//
+
+import XCTest
+@testable import csenseIosBase
+import csenseTests
+
+
+class csenseIosBaseTests: XCTestCase {
+    
+    func testMap() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let b = false
+        b.assert(false)
+        
+    }
+    
+    
+}
